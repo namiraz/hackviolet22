109f3a6a2fe8fe608685002d66c14913be807102
SharifAlvi <98751841+SharifAlvi@users.noreply.github.com>
2022-02-06 00:43:26 -0500

    API Get Code

--- recipeapp/routes.py
+++ recipeapp/routes.py
@@ -10,6 +10,17 @@ def home():
         session["ingredients"] = form.ingredients.data
         session["appliances"] = form.appliances.data
         #API call below this point
+        url = "https://tasty.p.rapidapi.com/recipes/list"
+
+        querystring = {"from":"0","size":"5","q":form.ingredients.data}
+
+        headers = {
+            'x-rapidapi-host': "tasty.p.rapidapi.com",
+            'x-rapidapi-key': "fe5e900e75msh990876c1b7db3bap12a124jsn651c3ff6276b"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        j = response.text
         return redirect(url_for("results"))
     return render_template("home.html", form=form)
 
