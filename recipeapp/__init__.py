e60c5133f0997d5928d89860fcfb55c3071e3f27
Reanna Ishmael <rishmael@fordham.edu>
2022-02-06 00:10:24 -0500

    Changes to routes

--- recipeapp/__init__.py
+++ recipeapp/__init__.py
@@ -1,9 +1,14 @@
 import os
 
 from flask import Flask
+from flask_session import Session
 
 app = Flask(__name__)
 secret_key = os.getenv("SECRET_KEY")
 app.config["SECRET_KEY"] = secret_key
+
+SESSION_TYPE = "filesystem"
+app.config.from_object(__name__)
+Session(app)
 
 from recipeapp import routes
--- recipeapp/routes.py
+++ recipeapp/routes.py
@@ -1,17 +1,28 @@
-from flask import render_template, url_for
+from flask import render_template, redirect, session, url_for
 from recipeapp import app
 from recipeapp.forms import IngredientsListForm
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 def home():
     form = IngredientsListForm()
+    if form.validate_on_submit():
+        session["ingredients"] = form.ingredients.data
+        session["appliances"] = form.appliances.data
+        #API call below this point
+        return redirect(url_for("results"))
     return render_template("home.html", form=form)
 
-@app.route("/manual")
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    ingredients = session["ingredients"]
+    appliances = session["appliances"]
+    return render_template("results.html", ingredients=ingredients, appliances=appliances)
+
+@app.route("/manual", methods=["GET", "POST"])
 def manual():
     return render_template("manual.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     return render_template("contact.html")
